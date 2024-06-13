18088edfa5ca5081db277ba68d4c55b45d78f6ae
mohinigai <you@example.com>
2024-06-13 23:07:01 +0530

    commit for update controller user api

---
+++ src/main/java/com/example/service/UserServiceI.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import com.example.model.User;
+
+import java.util.List;
+
+public interface UserServiceI {
+    User createUser(User user);
+
+    User updateUser(User user,Long userId);
+
+    User getsingleUser(Long userId) throws Exception;
+
+//    List<User> getAllUsers();
+    List<User> getAllUsers();
+
+    void deleteUser(Long userId);
+
+
+}
---
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.example.service;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserServiceI {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        User saveduser = userRepository.save(user);
+        return saveduser;
+    }
+
+    @Override
+    public User updateUser(User user, Long userId) {
+
+        User user1 = userRepository.findById(userId).get();
+        user1.setUserName(user.getUserName());
+        user1.setUserAge(user.getUserAge());
+        user1.setAbout(user.getAbout());
+
+        User updatedUser = userRepository.save(user1);
+        return updatedUser;
+    }
+
+    @Override
+    public User getsingleUser(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Resource not found on server!!" + userId));
+        return user;
+            /* Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            return user.get();
+        }else {
+            throw new Exception("Resource not found on server!! "+user);
+        }
+    }*/
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> allusers = userRepository.findAll();
+        return allusers;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Resource not found on server!!" + userId));
+
+        userRepository.delete(user);
+    }
+
+}
+
+
+
